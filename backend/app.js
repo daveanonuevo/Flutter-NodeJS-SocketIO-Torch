24cd7fb00c3e3f3ab1dca924645d3070c33de339
Dave Anonuevo <dave7999matthew@gmail.com>
2020-02-07 19:54:09 +0800

    Bug Fix

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const socket_io = require('socket.io');
